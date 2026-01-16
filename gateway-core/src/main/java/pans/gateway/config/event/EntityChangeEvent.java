7fcb2d054abe3959df7bdc52206b7fe4ad3cadce
zhiwen.tan <zhiwen.t@outlook.com>
2026-01-16 22:16:28 +0800

    重构后第一版基础功能可用代码，但仍存在一些已知问题。 1. 并发限制为5，并发实际为5也出现连接限制告警日志 2. HealthCheckManager.startBackendChecking未执行

---
+++ gateway-core/src/main/java/pans/gateway/config/event/EntityChangeEvent.java
@@ -0,0 +1,30 @@
+package pans.gateway.config.event;
+
+/**
+ * Base class for entity change events
+ */
+public abstract class EntityChangeEvent {
+    private final long version;
+    private final long timestamp;
+
+    protected EntityChangeEvent(long version) {
+        this.version = version;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    /**
+     * Get the configuration version
+     * @return The version number
+     */
+    public long getVersion() {
+        return version;
+    }
+
+    /**
+     * Get the timestamp when this event was created
+     * @return The timestamp in milliseconds
+     */
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
